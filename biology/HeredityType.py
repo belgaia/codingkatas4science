c934a398bac97da2cce014cf8876a9f2b6dfaeb2
Isabel Batista <isabelbatista@gmx.de>
2014-04-28 19:35:18 +0200

    Added heredity type to the mendel rules.

---
+++ biology/HeredityType.py
@@ -0,0 +1,4 @@
+__author__ = 'Agile Developers'
+
+class HeredityType():
+    INTERMEDIUS = "intermedius"
--- biology/mendelRulesCalculator.py
+++ biology/mendelRulesCalculator.py
@@ -1,18 +1,20 @@
 __author__ = 'Agile Developers'
 from biology.BlossomColor import BlossomColor
+from biology.HeredityType import HeredityType
 
-def calculateF1Generation(parentOne, parentTwo):
+def calculateF1Generation(heredityType, parentOne, parentTwo):
 
-    if parentOne == BlossomColor.WHITE:
-        if parentTwo == BlossomColor.RED:
-            return BlossomColor.PINK
-    elif parentOne == BlossomColor.RED:
-        if parentTwo == BlossomColor.WHITE:
-            return BlossomColor.PINK
-        elif parentTwo == BlossomColor.BLUE:
-            return BlossomColor.PURPLE
-    elif parentOne == BlossomColor.BLUE:
-        if parentTwo == BlossomColor.RED:
-            return BlossomColor.PURPLE
+    if heredityType == HeredityType.INTERMEDIUS:
+        if parentOne == BlossomColor.WHITE:
+            if parentTwo == BlossomColor.RED:
+                return BlossomColor.PINK
+        elif parentOne == BlossomColor.RED:
+            if parentTwo == BlossomColor.WHITE:
+                return BlossomColor.PINK
+            elif parentTwo == BlossomColor.BLUE:
+                return BlossomColor.PURPLE
+        elif parentOne == BlossomColor.BLUE:
+            if parentTwo == BlossomColor.RED:
+                return BlossomColor.PURPLE
 
     return BlossomColor.UNDEFINED
