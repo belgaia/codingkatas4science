336a34e45d792350d825ed43186a87c25699912c
Isabel Batista <isabelbatista@gmx.de>
2014-05-02 22:13:03 +0200

    Refactored code to return full color instead of just one or two values.

--- helpers/colorCalculator.py
+++ helpers/colorCalculator.py
@@ -4,6 +4,9 @@ import csv
 import os
 
 file = "C:/Users/Agile Developers/PycharmProjects/codingkatas4science/resources/" + 'colorhexrgbtable.csv'
+RED = "red"
+GREEN = "green"
+BLUE = "blue"
 
 def getColorByHex(hexvalue):
 
@@ -19,7 +22,7 @@ def getColorByHex(hexvalue):
                     return foundColor
             lineCounter = lineCounter +1
 
-def getColorByRGB(red, yellow, blue):
+def getColorByRGB(red, green, blue):
 
     with open(file, newline='') as csvfile:
 
@@ -28,7 +31,7 @@ def getColorByRGB(red, yellow, blue):
         for row in colorReader:
 
             if lineCounter > 1:
-                foundColor = __findRgbColor(red, yellow, blue, row)
+                foundColor = __findRgbColor(red, green, blue, row)
                 if foundColor != None:
                     return foundColor
             lineCounter = lineCounter +1
@@ -46,9 +49,30 @@ def getRgbAndHexByColorname(colorname):
                 if foundColorInformation != None:
                     return foundColorInformation
             lineCounter = lineCounter +1
+'''
+def calculateUniColorMixByPercent(colorInRgb, colorToCalculate, percent):
 
+    # 255,255,255 + 0,0,255
 
-def __findRgbColor(red, yellow, blue, colorLine):
+    #Hex-Wert 	RGB-Anteil 	Leuchtkraft (%)
+    #00 	        0        	0 %
+    #40 	        64 	        25 %
+    #80 	        128 	    50 %
+    #C0 	        192 	    75 %
+    #FF 	        255 	    100 %
+
+    rgbToken = colorInRgb.split(',')
+
+    colorInfo = {}
+    if colorToCalculate == BLUE:
+        colorInfo['colorname'] = getColorByRGB(rgbToken[0], rgbToken[1], (rgbToken[2] * 0,5))
+        colorInfo['']
+
+'''
+
+def __findRgbColor(red, green, blue, colorLine):
+
+    color = {}
 
     colorname = colorLine[0]
     rgb = colorLine[1]
@@ -56,19 +80,25 @@ def __findRgbColor(red, yellow, blue, colorLine):
 
     rgbTokens = rgb.split(",")
     redFromFile = rgbTokens[0]
-    yellowFromFile = rgbTokens[1]
+    greenFromFile = rgbTokens[1]
     blueFromFile = rgbTokens[2]
 
-    if (redFromFile == str(red)) & (yellowFromFile == str(yellow)) & (blueFromFile == str(blue)):
-        return colorname
+    if (redFromFile == str(red)) & (greenFromFile == str(green)) & (blueFromFile == str(blue)):
+        color['name'] = colorname
+        color['rgb'] = colorLine[1]
+        color['hex'] = colorLine[2]
+        return color
 
 def __findHexColor(hexvalue, colorLine):
 
-    colorname = colorLine[0]
     hexFromFile = colorLine[2]
 
+    color = {}
     if hexvalue == hexFromFile:
-        return colorname
+        color['name'] = colorLine[0]
+        color['rgb'] = colorLine[1]
+        color['hex'] = hexFromFile
+        return color
 
 def __findColor(colorname, colorLine):
 
@@ -79,5 +109,5 @@ def __findColor(colorname, colorLine):
         colorRgbAndHex = {}
         colorRgbAndHex['rgb'] = colorLine[1]
         colorRgbAndHex['hex'] = colorLine[2]
-        print(colorRgbAndHex)
+        colorRgbAndHex['name'] = colorname
         return colorRgbAndHex
--- tests/helpers/testColorCalculator.py
+++ tests/helpers/testColorCalculator.py
@@ -8,15 +8,32 @@ class TestFileReader(TestCase):
     def testGettingRgbColor(self):
 
         foundColor = helpers.colorCalculator.getColorByRGB(240, 248, 255)
-        self.assertEquals("AliceBlue", foundColor)
+        self.assertEquals("AliceBlue", foundColor['name'])
+        self.assertEquals("F0F8FF", foundColor['hex'])
+        self.assertEquals("240,248,255", foundColor['rgb'])
 
     def testGettingHexColor(self):
 
         foundColor = helpers.colorCalculator.getColorByHex("F0F8FF")
-        self.assertEquals("AliceBlue", foundColor)
+        self.assertEquals("AliceBlue", foundColor['name'])
+        self.assertEquals("F0F8FF", foundColor['hex'])
+        self.assertEquals("240,248,255", foundColor['rgb'])
 
     def testGettingRgbAndHexForColorname(self):
 
         foundRgbAndHex = helpers.colorCalculator.getRgbAndHexByColorname("AliceBlue")
         self.assertEquals("240,248,255", foundRgbAndHex["rgb"])
         self.assertEquals("F0F8FF", foundRgbAndHex["hex"])
+        self.assertEquals("AliceBlue", foundRgbAndHex["name"])
+
+    '''
+    skip("first refactor other tests")
+    def testCalculateColorMix(self):
+
+        blue = "0,0,255"
+        percent = 50
+        foundColor = helpers.colorCalculator.calculateUniColorMixByPercent(blue, "blue", percent)
+
+        self.assertEquals("0,0,128", foundColor['rgb'])
+
+    '''
